9560a8f8bbd2a910808573db89d49c77ea03c232
Caleb Britannia <calebbritannia@gmail.com>
2021-04-15 13:46:10 +0100

    Getting the plugin environment set up

---
+++ src/main/java/com/toffydungeons/ToffyDungeons.java
@@ -0,0 +1,16 @@
+package com.toffydungeons;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ToffyDungeons extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        System.out.println("Working");
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+}
